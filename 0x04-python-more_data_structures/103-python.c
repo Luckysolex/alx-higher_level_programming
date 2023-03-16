68d90bc1b5dc6c8851d24e3b0fa55c47edb24ff0
Luckysolex <Luckysolex1@gmail.com>
2023-03-15 19:43:07 -0700

    We created two C functions that print some basic info about Python lists and Python bytes objects

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -3,8 +3,9 @@
 
 /**
  * print_python_bytes - Prints bytes information
- * @p: python object
- * Return: void
+ *
+ * @p: Python Object
+ * Return: no return
  */
 void print_python_bytes(PyObject *p)
 {
@@ -14,28 +15,29 @@ void print_python_bytes(PyObject *p)
 	printf("[.] bytes object info\n");
 	if (!PyBytes_Check(p))
 	{
-		printf(" [ERROR] Invalid Bytes Object\n");
+		printf("  [ERROR] Invalid Bytes Object\n");
 		return;
 	}
 
 	size = ((PyVarObject *)(p))->ob_size;
 	string = ((PyBytesObject *)p)->ob_sval;
 
-	printf(" size: %ld\n", size);
-	printf(" trying strin: %s\n", string);
+	printf("  size: %ld\n", size);
+	printf("  trying string: %s\n", string);
 
 	if (size >= 10)
 		limit = 10;
 	else
 		limit = size + 1;
-	
-	printf(" first %ld bytes:", limit);
+
+	printf("  first %ld bytes:", limit);
 
 	for (i = 0; i < limit; i++)
 		if (string[i] >= 0)
 			printf(" %02x", string[i]);
 		else
 			printf(" %02x", 256 + string[i]);
+
 	printf("\n");
 }
 
