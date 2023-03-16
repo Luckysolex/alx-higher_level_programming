50a4894bba7cb053ce0c6eeb329ddfca44c267fd
Luckysolex <Luckysolex1@gmail.com>
2023-03-15 18:40:35 -0700

    I don't know what the error is but I have to fix it

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def search_replace(my_list, search,replace):
+def search_replace(my_list, search, replace):
     new_list = []
     for index in my_list:
         if index != search:
