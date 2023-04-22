35565fab742ec4695608e44ef9dd4161d1bac512
Luckysolex <Luckysolex1@gmail.com>
2023-04-21 17:37:35 -0700

    safe print integer

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -2,10 +2,11 @@
 from __future__ import print_function
 import sys
 
+
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
-        retuen True
+        return True
     except Exception as e:
         print("Exception: {}".format(e), file=sys.stderr)
         return False
