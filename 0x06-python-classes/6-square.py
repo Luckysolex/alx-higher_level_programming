86a07694cd241acbb1700a433753375bb151d204
Luckysolex <Luckysolex1@gmail.com>
2023-05-03 07:40:11 -0700

    Coordinates of a square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -19,7 +19,7 @@ class Square:
         if not (isinstance(size, int)):
             raise TypeError("size must be an integer")
         if size < 0:
-            raise valueError("size must be >= 0")
+            raise ValueError("size must be >= 0")
 
         self.__size = size
         self.__position = position
