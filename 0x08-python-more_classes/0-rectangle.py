d235c678cd9157c0552ea5f8af914270cbc9b4c4
Luckysolex <Luckysolex1@gmail.com>
2023-06-07 01:46:38 -0700

    Still on rectangle

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
+"""0-rectangle, first task.
+"""
+
 
 class Rectangle:
-    """Rectangle class"""
+    """Rectangle class.
+    """
     pass
-
-
-if __name__ == '__main__':
-    my_rectangle = Rectangle()
-    print(type(my_rectangle))
-    print(my_rectangle.__dict__)
