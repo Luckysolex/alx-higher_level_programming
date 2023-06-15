f2261abc865021663c8044f211215b420de7d548
Luckysolex <Luckysolex1@gmail.com>
2023-06-14 18:20:28 -0700

    Square 2

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -33,7 +33,7 @@ class Square(Rectangle):
         Returns:
             str: square.
         """
-        return ("[square] {}/{}".format(self.__size, self.__size))
+        return ("[Square] {}/{}".format(self.__size, self.__size))
 
     def area(self):
         """Calculates the area of a square.
