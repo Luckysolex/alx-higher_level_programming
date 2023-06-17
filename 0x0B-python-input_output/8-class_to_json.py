7bee153f21567f8577ffbaff1e8a000536253706
Luckysolex <Luckysolex1@gmail.com>
2023-06-16 20:00:48 -0700

    Class to JSON

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""Module with the function that returns the dictionary description with simple data structure
-(list, dictionary, string, integer and boolean) for JSON serialization of an object."""
+"""Module with the function class_to_json."""
 
 
 def class_to_json(obj):
