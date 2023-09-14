0a1c46f2d81d2be698703b814fc8368cf13cefc0
Lucky <Luckysolex1@gmail.com>
2023-09-14 09:25:22 -0700

    file 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-for (const item of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
-	console.log(item);
+for (const line of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
+  console.log(line);
 }
