390aa79a39f0c251e94a22e9a24f5729844708e1
Lucky <luckysolex1@gmail.com>
2023-10-03 09:49:05 -0700

    first file

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,16 @@
+i#!/usr/bin/python3
+"""This Module contain a script that
+i. fetches https://alx-intranet.hbtn.io/status.
+ii. uses urlib package
+"""
+
+
+if __name__ == '__main__':
+    import urllib.request
+
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        content = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
