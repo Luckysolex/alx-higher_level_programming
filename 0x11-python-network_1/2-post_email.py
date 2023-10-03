978acbde3adb570bbdb084d5b51e4244996b05f9
Lucky <luckysolex1@gmail.com>
2023-10-03 10:24:30 -0700

    Mandatory tasks

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """
-This module contain a Python script that takes in a URL and an email, sends a POST
-request to the passed URL with the email as a parameter, and
-displays the body of the response
+This module contain a Python script that takes in a URL and an email,
+sends a POSTrequest to the passed URL with the email as a parameter,
+and displays the body of the response
 """
 from urllib import request, parse
 import sys
--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """
-This module contain a Python script that takes in a URL, sends a request and displays
-the value of the X-Request-Id variable found in the header
-(handling HTTP errors)
+This module contain a Python script that takes in a URL,
+sends a request and displaysthe value of the X-Request-Id
+variable found in the header (handling HTTP errors)
 """
 from urllib import request, error
 import sys
